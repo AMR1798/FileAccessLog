4ebebea063bbfb5fb5b0ea644ce663d603a63713
amrmzkr-pernec <ammarmuzakkir@pernec.com.my>
2023-11-30 09:43:59 +0800

    initialize

---
+++ lib/Service/NoteNotFound.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: AMRMZKR <ammar.muzakkir@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\FALog\Service;
+
+class NoteNotFound extends \Exception {
+}
